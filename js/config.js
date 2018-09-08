0a091e92a34cf2b48ff2072f024b0de765a9917a
Ramon Wilkinson <ramon.wilkinson@gmail.com>
2018-09-08 16:30:03 -0700

    add API call

--- js/config.js
+++ js/config.js
@@ -1,10 +1,18 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_PAUCl3r7z',
+        userPoolClientId: '1mrpnvhboldoh5pck00gk60ama',
+        region: 'us-east-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
+};
+
+var configConstants = {
+    auth0: {
+        domain: 'YOUR_AUTH0_DOMAIN_HERE',
+        clientId: 'YOUR_AUTH0_CLIENT_ID_HERE'
+    },
+    apiBaseUrl: 'https"//api.wilkinson.guru'
 };
