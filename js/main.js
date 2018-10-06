5086225aced4f42aad416e32fbecfb460ae5d7fd
Ramon Wilkinson <ramon.wilkinson@gmail.com>
2018-10-06 14:08:24 -0700

    add headers

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function () {
   $(document).ready(function () {
-      userController.init(configConstants);
       cognitoAuth.init();
-  });
+      userController.init(configConstants);
+    });
 }());
 
